0e6f46b3636c4e9ebacc0bf691b416b2d709903e
damian-rog <damian.rog@quantoo.pl>
2024-12-04 01:12:21 +0100

    fix: remove links from manageButtons component

--- components/SingleMenu/subcomponents/ManageButtons/ManageButtons.tsx
+++ components/SingleMenu/subcomponents/ManageButtons/ManageButtons.tsx
@@ -1,20 +1,22 @@
 import { Menu } from '@/lib/types';
 import './ManageButtons.scss';
-import { useContext } from 'react';
-import { AddMenuContext } from '@/app/page';
+import { useMenus } from '@/context/MenusContext';
+import { Dispatch, SetStateAction } from 'react';
 
 type Props = {
     menu: Menu,
+    setAddMenuFormVisibility: Dispatch<SetStateAction<boolean>>,
+    setEditMenuFormVisibility: Dispatch<SetStateAction<boolean>>
 }
 
-const ManageButtons = ({ menu }: Props) => {
-    const { deleteMenu } = useContext(AddMenuContext);
+const ManageButtons = ({ menu, setAddMenuFormVisibility, setEditMenuFormVisibility }: Props) => {
+    const { deleteMenu } = useMenus();
 
     return (
         <div className="manage--buttons">
             <button className="button -delete" onClick={() => deleteMenu(menu.id)}>Usuń</button>
-            <button className="button -edit">Edytuj</button>
-            <button className="button -add">Dodaj pozycję menu</button>
+            <button className="button -edit" onClick={() => { setEditMenuFormVisibility(prevVisibility => !prevVisibility) }}>Edytuj</button>
+            <button className='button -add' onClick={() => { setAddMenuFormVisibility(prevVisibility => !prevVisibility) }}>Dodaj pozycję menu</button>
         </div>
     )
 }
