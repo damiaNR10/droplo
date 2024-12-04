2854038eb6c77c8e0a4374347cfa485632792271
damian-rog <damian.rog@quantoo.pl>
2024-12-04 01:07:00 +0100

    fix: add id property to Menu type & create MenuContext type

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,15 @@
 type Menu = {
+  id: string;
   name: string;
   url: string;
   children?: Menu[];
 };
 
-export type { Menu };
+type MenuContextType = {
+  menusList: Menu[];
+  addMenu: (menu: Menu, parentId?: string) => void;
+  updateMenu: (id: string, newName: string, newUrl: string) => void;
+  deleteMenu: (id: string) => void;
+};
+
+export type { Menu, MenuContextType };
