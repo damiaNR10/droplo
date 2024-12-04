f10c210c63425d04585212f9098ca45029e378dd
damian-rog <damian.rog@quantoo.pl>
2024-12-04 01:43:57 +0100

    feat: add AddForm to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,31 @@
-import { Menu } from "@/lib/types";
-import EmptyList from "./components/EmptyList/EmptyList";
-import MenusList from "./components/MenusList/MenusList";
-
-const menusList: Menu[] = [
-  {
-    name: 'Promocje',
-    url: 'https://rc32141.redcart.pl/promocje',
-  },
-  {
-    name: 'Promocje2',
-    url: 'https://rc32141.redcart.pl/promocje2',
-  },
-];
+'use client'
+import EmptyList from "../components/EmptyList/EmptyList";
+import MenusList from "../components/MenusList/MenusList";
+import AddButton from "../components/AddButton/AddButton";
+import { useMenus } from "@/context/MenusContext";
+import AddForm from "@/components/AddForm/AddForm";
+import { useState } from "react";
 
 export default function Home() {
+  const { menusList } = useMenus();
+  const [addMenuFormVisibility, setAddMenuFormVisibility] = useState(false);
+
   return (
     <div className="container">
       {
         menusList.length > 0 ?
           <div className="main--content">
             <MenusList menus={menusList} />
+            {addMenuFormVisibility &&
+              <AddForm setAddMenuFormVisibility={setAddMenuFormVisibility} />}
+            <AddButton setAddMenuFormVisibility={setAddMenuFormVisibility} />
           </div>
           :
-          <EmptyList />
+          <>
+            <EmptyList setAddMenuFormVisibility={setAddMenuFormVisibility} />
+            {addMenuFormVisibility &&
+              <AddForm setAddMenuFormVisibility={setAddMenuFormVisibility} />}
+          </>
       }
     </div>
   );
