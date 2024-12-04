7fd2d6b1105f4a418e1bd41ccb5d39d3975e7ba6
damian-rog <damian.rog@quantoo.pl>
2024-12-04 01:13:02 +0100

    feat: add forms for edit and & creating child menu

--- components/SingleMenu/SingleMenu.tsx
+++ components/SingleMenu/SingleMenu.tsx
@@ -4,6 +4,8 @@ import MenuDetails from "./subcomponents/MenuDetails/MenuDetails";
 import IconMove from '../../public/img/icon-move.svg';
 import './SingleMenu.scss';
 import ManageButtons from "./subcomponents/ManageButtons/ManageButtons";
+import { useState } from "react";
+import AddForm from "../AddForm/AddForm";
 
 type Props = {
     menu: Menu,
@@ -11,6 +13,8 @@ type Props = {
 }
 
 const SingleMenu = ({ menu, menuLevel }: Props) => {
+    const [addMenuFormVisibility, setAddMenuFormVisibility] = useState(false);
+    const [editMenuFormVisibility, setEditFormMenuVisibility] = useState(false);
 
     return (
         <>
@@ -24,8 +28,16 @@ const SingleMenu = ({ menu, menuLevel }: Props) => {
                     />
                 </button>
                 <MenuDetails menu={menu} />
-                <ManageButtons menu={menu} />
+                <ManageButtons menu={menu} setAddMenuFormVisibility={setAddMenuFormVisibility} setEditMenuFormVisibility={setEditFormMenuVisibility} />
             </div>
+            {
+                addMenuFormVisibility &&
+                <AddForm menuLevel={menuLevel + 1} parentId={menu.id} setAddMenuFormVisibility={setAddMenuFormVisibility} />
+            }
+            {
+                editMenuFormVisibility &&
+                <AddForm menuLevel={menuLevel + 1} menu={menu} setEditFormMenuVisibility={setEditFormMenuVisibility} />
+            }
             {
                 menu.children && menu.children.length > 0 ?
                     menu.children.map((submenu) => <SingleMenu menuLevel={menuLevel + 1} key={submenu.id} menu={submenu} />)
