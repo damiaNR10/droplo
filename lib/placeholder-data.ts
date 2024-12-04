71346b6bd6f09b3f56bc430aeeec349ca393cbb1
damian-rog <damian.rog@quantoo.pl>
2024-12-04 01:43:30 +0100

    fix: set generated id for elements

--- lib/placeholder-data.ts
+++ lib/placeholder-data.ts
@@ -1,34 +1,29 @@
 import { Menu } from "./types";
-// import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 const menus: Menu[] = [
   {
-    //id: uuidv4(),
-    id: "1",
+    id: uuidv4(),
     name: "Promocje",
     url: "https://rc32141.redcart.pl/promocje",
     children: [
       {
-        //id: uuidv4(),
-        id: "2",
+        id: uuidv4(),
         name: "test",
         url: "https://rc32141.redcart.pl/test",
         children: [
           {
-            //id: uuidv4(),
-            id: "3",
+            id: uuidv4(),
             name: "test - nested",
             url: "https://rc32141.redcart.pl/nested",
             children: [
               {
-                //id: uuidv4(),
-                id: "4",
+                id: uuidv4(),
                 name: "test77",
                 url: "https://rc32141.redcart.pl/test2",
               },
               {
-                //id: uuidv4(),
-                id: "5",
+                id: uuidv4(),
                 name: "testx",
                 url: "https://rc32141.redcart.pl/test2",
               },
@@ -39,26 +34,22 @@ const menus: Menu[] = [
     ],
   },
   {
-    //id: uuidv4(),
-    id: "6",
+    id: uuidv4(),
     name: "Promocje2",
     url: "https://rc32141.redcart.pl/promocje2",
   },
   {
-    //id: uuidv4(),
-    id: "7",
+    id: uuidv4(),
     name: "Promocje3",
     url: "https://rc32141.redcart.pl/promocje2",
     children: [
       {
-        //id: uuidv4(),
-        id: "8",
+        id: uuidv4(),
         name: "test",
         url: "https://rc32141.redcart.pl/test",
         children: [
           {
-            //id: uuidv4(),
-            id: "9",
+            id: uuidv4(),
             name: "test - nested",
             url: "https://rc32141.redcart.pl/nested",
           },
