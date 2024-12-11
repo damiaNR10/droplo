c49722f3aacf3c3d0859427ba4f8efec67888547
damian-rog <damian.rog@quantoo.pl>
2024-12-11 22:04:30 +0100

    feat: drag and drop functionality (only for first-level elements yet)

--- components/MenusList/MenusList.tsx
+++ components/MenusList/MenusList.tsx
@@ -1,6 +1,9 @@
 import { Menu } from "@/lib/types";
 import SingleMenu from "../SingleMenu/SingleMenu";
 import './MenusList.scss';
+import { closestCenter, DndContext, DragEndEvent } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { useMenus } from "@/context/MenusContext";
 
 type Props = {
     menus: Menu[],
@@ -8,13 +11,29 @@ type Props = {
 
 const MenusList = ({ menus }: Props) => {
 
+    const { getMenuPosition, changeMenusOrder } = useMenus();
+
+    const handleDragEnd = (event: DragEndEvent): void => {
+        const { active, over } = event;
+        if (active.id !== over?.id) {
+            changeMenusOrder(getMenuPosition(active.id.toString()), getMenuPosition(over ? over.id.toString() : ''));
+        }
+    }
+
     return (
         <section className="menus--list">
-            {
-                menus.map((menu) => (
-                    <SingleMenu menuLevel={0} key={menu.id} menu={menu} />
-                ))
-            }
+            <DndContext
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
+            >
+                <SortableContext items={menus} strategy={verticalListSortingStrategy}>
+                    {
+                        menus.map((menu) => (
+                            <SingleMenu menuLevel={0} key={menu.id} menu={menu} />
+                        ))
+                    }
+                </SortableContext>
+            </DndContext>
         </section>
     )
 }
--- components/SingleMenu/SingleMenu.tsx
+++ components/SingleMenu/SingleMenu.tsx
@@ -6,6 +6,8 @@ import './SingleMenu.scss';
 import ManageButtons from "./subcomponents/ManageButtons/ManageButtons";
 import { useState } from "react";
 import AddForm from "../AddForm/AddForm";
+import { SortableContext, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities"
 
 type Props = {
     menu: Menu,
@@ -15,10 +17,16 @@ type Props = {
 const SingleMenu = ({ menu, menuLevel }: Props) => {
     const [addMenuFormVisibility, setAddMenuFormVisibility] = useState(false);
     const [editMenuFormVisibility, setEditFormMenuVisibility] = useState(false);
+    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: menu.id });
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    };
 
     return (
         <>
-            <div className={`single--menu -level${menuLevel}`}>
+
+            <div ref={setNodeRef} style={style} {...attributes} {...listeners} className={`single--menu -level${menuLevel}`}>
                 <button className="move">
                     <Image
                         src={IconMove}
@@ -38,11 +46,17 @@ const SingleMenu = ({ menu, menuLevel }: Props) => {
                 editMenuFormVisibility &&
                 <AddForm menuLevel={menuLevel + 1} menu={menu} setEditFormMenuVisibility={setEditFormMenuVisibility} />
             }
+
+
             {
                 menu.children && menu.children.length > 0 ?
-                    menu.children.map((submenu) => <SingleMenu menuLevel={menuLevel + 1} key={submenu.id} menu={submenu} />)
+                    <SortableContext items={menu.children} strategy={verticalListSortingStrategy}>
+                        {menu.children.map((submenu) => <SingleMenu menuLevel={menuLevel + 1} key={submenu.id} menu={submenu} />)}
+                    </SortableContext>
                     : ''
             }
+
+
         </>
     )
 }
