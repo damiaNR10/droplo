4d00e9a67be88a971a8bcdb9a41c73a83a87c88e
damian-rog <damian.rog@quantoo.pl>
2024-12-16 23:59:15 +0100

    feat: add type for flatten array

--- lib/types.ts
+++ lib/types.ts
@@ -1,17 +1,25 @@
+import { UniqueIdentifier } from "@dnd-kit/core";
+
 type Menu = {
   id: string;
   name: string;
   url: string;
   children?: Menu[];
+  collapsed?: boolean;
 };
+
+interface FlattenedMenu extends Menu {
+  parentId: UniqueIdentifier | null;
+  depth: number;
+  index: number;
+}
 
 type MenuContextType = {
   menusList: Menu[];
   addMenu: (menu: Menu, parentId?: string) => void;
   updateMenu: (id: string, newName: string, newUrl: string) => void;
   deleteMenu: (id: string) => void;
-  getMenuPosition: (id: string) => number;
-  changeMenusOrder: (active: number, over: number) => void;
+  setMenusList: React.Dispatch<React.SetStateAction<Menu[]>>;
 };
 
-export type { Menu, MenuContextType };
+export type { Menu, MenuContextType, FlattenedMenu };
